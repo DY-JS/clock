d65abeb63f88e3eb73524194f0c549592ab968e9
DY-JS <delo1604@ukr.net>
2023-08-06 18:13:56 +0200

    Add clock

---
+++ src/Clock/Clock.tsx
@@ -0,0 +1,26 @@
+import React, {useEffect, useState} from 'react';
+import cls from "./clock.module.css"
+
+const Clock = () => {
+    const [date, setDate] = useState(new Date())
+
+    const transformDigits = (value: number) => {
+        return value < 10 ? '0' + value : value
+    }
+
+    useEffect(() => {
+        const interval = setInterval(()=> setDate(new Date()), 1000);
+
+        return () => clearInterval(interval)
+    }, [])
+
+    return (
+        <div className={cls.clock}>
+            <span>{transformDigits(date.getHours())}:</span>
+            <span>{transformDigits(date.getMinutes())}:</span>
+            <span>{transformDigits(date.getSeconds())}</span>
+        </div>
+    );
+};
+
+export default Clock;
